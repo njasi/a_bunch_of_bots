355b23e51aace42617bf01c3cbd1ba22821be9e5
Archie Shahidullah <35503985+Archiecool4@users.noreply.github.com>
2019-02-23 17:17:39 -0800

    Added Genius Functionality

--- RichardFeynman.py
+++ RichardFeynman.py
@@ -1,23 +1,28 @@
-import json 
+import json
 import requests
 import time
 import urllib
 import string
 import random
+import wolframalpha
+import wikipedia
+import warnings
+import os
+
 TOKEN = "607870320:AAHM64JdBsWIZhtfYpw-o8kNjGnwI-UC7dw"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+app_id = "VKA5LT-H9GK59QQYJ"
+client = wolframalpha.Client(app_id)
 
 TRIGGER_STICKERS = [
     ('CAADAwADxAAD3zLTBALNnvfeN-vcAg',['no u']),
     ('StickerId2Here',['list','of','possible','responses2'])
 ]
 
-
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
-
 
 def get_json_from_url(url):
     content = get_url(url)
@@ -44,15 +49,13 @@ def get_last_chat_id_and_text(updates):
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
 
-
 def send_message(text, chat_id):
     text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
-    
-
 def main():
+    warnings.filterwarnings('ignore', category=UserWarning)
     last_update_id = None
     while True:
         updates = get_updates(last_update_id)
@@ -66,45 +69,35 @@ def respond(updates):
         try:
             for response in TRIGGER_STICKERS:
                 respond_trigger_stickers(update, response[1], stickerid = response[0])
-            #respond_trigger_words_user(update,True,[["heck","anus","arse","arsehole","ass","bitch","inappropriate","inappropriate","inappropriate","cocksucking","coochie","coddamnit","coochy","cooter","cum","cumbubble","cumdumpster","cummer","cumming","cumshot","cumslut","cuntface","cunthole","cuntlick","cuntlicker","inappropriate","dickbag","dickbeaters","dickface","dickfuck","dickhead","dickhole","dickjuice","dickmilk","dickslap","dickwad","dickweasel","dickweed","dickwod","dildo","dink","inappropriate","inappropriate","inappropriate","inappropriate","inappropriate","hayward","fag","freaking","faggot","fuck","fuckersucker","fuckface","fuckhead","fuckhole","fuckin","fucking","fuckme","fucknut","fucknutt","fuckoff","fuckstick","fucktard","fuckup","fuckwad","fuckwit","fuckwitt","fudgepacker","fuk","gangbang","gangbanged","goddamn","blasphemy","inappropriate","gooch","gook","gringo","guido","handjob","hardcoresex","heeb","hell","ho","hoe","homo","homodumbshit","honkey","horniest","horny","hotsex","humping","jackass","jap","jigaboo","jism","jiz","jizm","jizz","jungle bunny","junglebunny","kike","kock","kondum","kooch","kootch","kum","kumer","kummer","kumming","kums","kunilingus","kunt","kyke","lezzie","lust","lusting","mcfagget","mick","minge","mothafuck","mothafucka","mothafuckin","mothafucking","mothafucks","motherfuck","motherfucked","motherfucker","munging","nut","nutt","negro","nigga","nigger","niglet","nut sack","nutsack","orgasim","orgasm","paki","panooch","pecker","peckerhead","penis","penisfucker","penispuffer","phonesex","phuk","phuked","phuking","phukked","phukking","phuks","phuq","pis","pises","pisin","pising","pisof","piss","pissed","pisser","pisses","pissflaps","pissin","pissing","pissoff","polesmoker","pollock","poon","poonani","poonany","poontang","porch monkey","porchmonkey","porn","porno","pornography","pornos","prick","punanny","punta","pusies","pussy","shitass","shitbag","shitbagger","shitbrain","shitbreath","shitcunt","shitdick","shited","shitface","shitfaced","inappropriate","inappropriate","shitted","shitter","shittiest","shitting","shitty","shity","shiz","shiznit","skank","sex","skeet","skullfuck","slut","slutbag","sluts","smeg","smut","snatch","spic","spick","splooge","spunk","tard","testicle","thundercunt","tit","tits","titfuck","tittyfuck","twat","twatlips","twatwaffle","unclefucker","va-j-j","vag","vagina","vjayjay","wank","wetback","whore","whorebag","whoreface","jew","jews","anal","anus","arse","ass","ass fuck","ass hole","assfucker","asshole","assshole","bastard","bitch","black cock","bloody hell","boong","cock","cockfucker","cocksuck","cocksucker","coon","coonnass","crap","cunt","cyberfuck","damn","darn","dick","dirty","douche","dummy","erect","erection","erotic","escort","fag","faggot","fuck","Fuck off","fuck you","fuckass","fuckhole","god damn","gook","hardcore","hardcore","homoerotic","hore","lesbian","lesbians","mother fucker","motherfuck","motherfucker","negro","nigger","orgasim","orgasm","penis","penisfucker","piss","piss off","porn","porno","pornography","pussy","retard","sadist","sex","sexy","shit","slut","son of a bitch","suck","tits","viagra","whore","xxx"]],["Watch your language!","Watch your language you stupid bitch!","Watch your language young lady!","Watch yourself brown man!"],[689166454,696039268,496220023])
             #respond_trigger_words(update,False,[["dinner","lunch","breakfast","dessert","supper"],"what",["I don't know, ask your mother.","How would I know?","You know I can't cook."])
             respond_trigger_words(update,False,[["daddy"]],["You called for me?"])
+            respond_smart(update)
+            os.remove('out.jpg')
         except Exception as e:
             # print(e)
             return
 
-
-'''
-def im_statements(update):
-    triggers=[]
+def respond_smart(update):
     text = update["message"]["text"]
+    if not "Feynman: " in text:
+        return
+    text = text[9:]
     chat = update["message"]["chat"]["id"]
+    r = search(text)
+    if r[0] == None:
+        send_message('That\'s a dumb question.')
+        return
+    send_message(r[0], chat)
+    if r[1] != None:
+        f = open('out.jpg','wb')
+        f.write(urllib.request.urlopen(r[1]).read())
+        f.close()
+        url = "https://api.telegram.org/bot" + TOKEN + "/sendPhoto";
+        files = {'photo': open('out.jpg', 'rb')}
+        data = {'chat_id' : chat}
+        r= requests.post(url, files=files, data=data)
+        os.remove('out.jpg')
 
-    def addtextafter(target,text):
-        things = []
-        while target in text.split():
-            text = text[text.find(target)+len(target):len(text)]
-            things += [text]
-        return things
-    # no '
-    triggers+=addtextafter('im',text)
-    triggers+=addtextafter('IM',text)
-    triggers+=addtextafter('iM',text)
-    triggers+=addtextafter('Im',text)
-    # weird ’
-    triggers+=addtextafter('i’m',text)
-    triggers+=addtextafter('I’M',text)
-    triggers+=addtextafter('i’M',text)
-    triggers+=addtextafter('I’m',text)
-    # normal '
-    triggers+=addtextafter("i'm",text)
-    triggers+=addtextafter("I'M",text)
-    triggers+=addtextafter("i'M",text)
-    triggers+=addtextafter("I'm",text)
-    for response in triggers:
-        if len(response) != 0:
-            send_message("Hi {}, I'm dad.".format(response.strip(" ")),chat)
-'''
 def respond_trigger_stickers(update, responses, stickerid = -1, packid = -1):
     try:
         sticker = update['message']['sticker']['file_id']
@@ -137,7 +130,7 @@ def respond_trigger_words(update,tokenize,triggers,responses):
     if b:
         send_message(responses[random.randint(0,len(responses)-1)],chat)
         raise Exception('a response was triggered')
-            
+
 def respond_trigger_words_user(update,tokenize,triggers,responses,userid):
     text = strip_punct(update["message"]["text"])
     if tokenize:
@@ -158,15 +151,68 @@ def respond_trigger_words_user(update,tokenize,triggers,responses,userid):
         except Exception as e:
             send_message(responses[0],chat)
         raise Exception('a response was triggered')
-        
-
 
 def strip_punct(text):
     exclude=set(string.punctuation)
     exclude.add('’')
-
     return (''.join(ch for ch in text if ch not in exclude)).lower()
 
+def search(text=''):
+  res = client.query(text)
+  if res['@success'] == 'false':
+     return search_wiki(text), primaryImage(text)
+  else:
+    result = ''
+    pod0 = res['pod'][0]
+    pod1 = res['pod'][1]
+    if (('definition' in pod1['@title'].lower()) or ('result' in  pod1['@title'].lower()) or (pod1.get('@primary','false') == 'true')):
+      result = resolveListOrDict(pod1['subpod'])
+      question = resolveListOrDict(pod0['subpod'])
+      question = removeBrackets(question)
+      return result, primaryImage(question)
+    else:
+      question = resolveListOrDict(pod0['subpod'])
+      question = removeBrackets(question)
+      return search_wiki(question), primaryImage(question)
+
+def search_wiki(query):
+    result = wikipedia.search(query)
+    if not result:
+        return
+    try:
+        page = wikipedia.page(query)
+        answer = wikipedia.summary(query, sentences=2)
+        if '==' in answer:
+            answer = wikipedia.summary(query, sentences=1)
+    except wikipedia.DisambiguationError as err:
+        page = wikipedia.page(err.options[0])
+        answer = wikipedia.summary(err.options[0], sentences=2)
+        if '==' in answer:
+            answer = wikipedia.summary(query, sentences=1)
+    except wikipedia.PageError as err:
+        answer = None
+    
+    return answer
+
+def removeBrackets(variable):
+    return variable.split('(')[0]
+
+def resolveListOrDict(variable):
+    if isinstance(variable, list):
+        return variable[0]['plaintext']
+    else:
+        return variable['plaintext']
+
+def primaryImage(title=''):
+    url = 'http://en.wikipedia.org/w/api.php'
+    data = {'action':'query', 'prop':'pageimages','format':'json','piprop':'original','titles':title}
+    try:
+        res = requests.get(url, params=data)
+        key = list(res.json()['query']['pages'].keys())[0]
+        imageUrl = res.json()['query']['pages'][key]['original']['source']
+        return imageUrl
+    except Exception as err:
+        return None
 
 if __name__ == '__main__':
     main()
